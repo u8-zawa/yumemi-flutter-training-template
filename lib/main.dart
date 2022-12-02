f5170e60b42fa56a96bd86d2ce698e03517946f9
u8-zawa <cist.b220.y.aizawa@gmail.com>
2022-12-02 18:33:05 +0900

    Add Text under Placeholder #2

--- lib/main.dart
+++ lib/main.dart
@@ -25,12 +25,41 @@ class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final deviceWidth = MediaQuery.of(context).size.width;
+    final labelLarge = Theme.of(context).textTheme.labelLarge!;
     return Scaffold(
       body: Center(
-        child: SizedBox(
-          width: deviceWidth / 2,
-          height: deviceWidth / 2,
-          child: const Placeholder(),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SizedBox(
+              width: deviceWidth / 2,
+              height: deviceWidth / 2,
+              child: const Placeholder(),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  alignment: Alignment.center,
+                  padding: const EdgeInsets.symmetric(vertical: 16),
+                  width: deviceWidth / 4,
+                  child: Text(
+                    '** ℃',
+                    style: labelLarge.copyWith(color: Colors.blue),
+                  ),
+                ),
+                Container(
+                  alignment: Alignment.center,
+                  padding: const EdgeInsets.symmetric(vertical: 16),
+                  width: deviceWidth / 4,
+                  child: Text(
+                    '** ℃',
+                    style: labelLarge.copyWith(color: Colors.red),
+                  ),
+                ),
+              ],
+            )
+          ],
         ),
       ),
     );
